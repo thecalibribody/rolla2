805be7c62e604fb4c931d5a3ec3909bd7a58881e
wincalibri <thecalibribody@users.noreply.github.com>
2023-02-28 22:39:48 +1100

    Add code to test rollparse

---
+++ test/rollparse-test.js
@@ -0,0 +1,18 @@
+var assert = requre('assert')
+
+describe('Argument Text Parser', ()=>{
+    it ('Can parse 1d6', ()=>{
+        let expected = [1,6];
+        let input = '1d6';
+    });
+    it ('Can parse 3d6', ()=>{
+        let expected = [3,6];
+        let input = '3d6';
+
+    });
+    it ('Can parse 2d20', ()=>{
+        let expected = [2,20];
+        let input = '2d20';
+
+    });
+});
