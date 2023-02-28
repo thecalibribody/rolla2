d1d2a28ff0206deb7447e594a2fde5b46440185b
wincalibri <thecalibribody@users.noreply.github.com>
2023-02-28 23:11:09 +1100

    Finalize rollparse-test

--- test/rollparse-test.js
+++ test/rollparse-test.js
@@ -1,18 +1,27 @@
-const assert = requre('assert')
+const assert = require('assert');
+const parseArgument = require('../src/rollparse.js')
 
 describe('Argument Text Parser', ()=>{
     it ('Can parse 1d6', ()=>{
-        let expected = [1,6];
+        let expected = ['1','6'];
         let input = '1d6';
+
+        const result = parseArgument(input);
+
+        assert.deepEqual(result,expected);
     });
     it ('Can parse 3d6', ()=>{
-        let expected = [3,6];
+        let expected = ['3','6'];
         let input = '3d6';
-
+        const result = parseArgument(input);
+        assert.deepEqual(result,expected);
     });
     it ('Can parse 2d20', ()=>{
-        let expected = [2,20];
+        let expected = ['2','20'];
         let input = '2d20';
+        const result = parseArgument(input);
+
+        assert.deepEqual(result,expected);
 
     });
 });
