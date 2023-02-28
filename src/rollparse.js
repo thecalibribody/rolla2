f43a40aef163b721af2d9f037ecbf36a045651bc
wincalibri <thecalibribody@users.noreply.github.com>
2023-02-28 23:42:56 +1100

    remove redundant code

--- src/rollparse.js
+++ src/rollparse.js
@@ -1,8 +1,5 @@
 function parseArgument(strArg){
     const splitString = strArg.split('d');
-    for (let i = 0; i<2; i++){
-        splitString[i]=splitString[Number(i)]
-    }
     return splitString;
 }
 
