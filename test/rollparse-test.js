bb3a0f56a987c39e1fce2c9b38d5d0b89a08e921
wincalibri <thecalibribody@users.noreply.github.com>
2023-02-28 23:15:45 +1100

    Made rollparse test more indicitave of expected output

--- test/rollparse-test.js
+++ test/rollparse-test.js
@@ -3,7 +3,7 @@ const parseArgument = require('../src/rollparse.js')
 
 describe('Argument Text Parser', ()=>{
     it ('Can parse 1d6', ()=>{
-        let expected = ['1','6'];
+        let expected = [1,6];
         let input = '1d6';
 
         const result = parseArgument(input);
@@ -11,13 +11,13 @@ describe('Argument Text Parser', ()=>{
         assert.deepEqual(result,expected);
     });
     it ('Can parse 3d6', ()=>{
-        let expected = ['3','6'];
+        let expected = [3,6];
         let input = '3d6';
         const result = parseArgument(input);
         assert.deepEqual(result,expected);
     });
     it ('Can parse 2d20', ()=>{
-        let expected = ['2','20'];
+        let expected = [2,20];
         let input = '2d20';
         const result = parseArgument(input);
 
